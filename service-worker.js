49c9eaeafb9ce8a688c86331a9728beb684d55b3
FerConejo <114848065+FerConejo@users.noreply.github.com>
2025-07-19 20:40:40 -0600

    act1

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'numeros-1-al-100-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './icon-192.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.2/p5.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
